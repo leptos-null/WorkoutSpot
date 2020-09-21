ea07191fc0d2106cf7afcef1a45146c59cd4a951
Leptos <leptos.0.null@gmail.com>
2020-09-21 11:03:00 -0700

    [WSCoordinateAnalysis] Review and optimize Accelerate usage

--- WorkoutSpot/Models/DataProcessing/WSCoordinateAnalysis.m
+++ WorkoutSpot/Models/DataProcessing/WSCoordinateAnalysis.m
@@ -59,33 +59,33 @@
     float *radii = malloc(length * sizeof(float));
     vDSP_vdpsp(altitudes.data, 1, radii, 1, length);
     float radius = 6378137; // https://en.wikipedia.org/wiki/Earth_radius#Geocentric_radius
-    vDSP_vsadd(radii, 1, &radius, radii, 1, length);
+    vDSP_vsadd(radii, 1, &radius, radii, 1, length); // radii += radius
     
     // https://en.wikipedia.org/wiki/Spherical_coordinate_system#Cartesian_coordinates
-    float *latRad = malloc(len * sizeof(float)); // theta
-    float *lngRad = malloc(len * sizeof(float)); // phi
+    float *latRad = malloc(length * sizeof(float)); // theta
+    float *lngRad = malloc(length * sizeof(float)); // phi
     
     // convert from degrees to radians,
     //   and seperate latitudes and longitudes
     float degToRad = M_PI / 180.0;
-    vDSP_vsmul(floatCoords + 0, 2, &degToRad, latRad, 1, len);
-    vDSP_vsmul(floatCoords + 1, 2, &degToRad, lngRad, 1, len);
+    vDSP_vsmul(floatCoords + 0, 2, &degToRad, latRad, 1, length);
+    vDSP_vsmul(floatCoords + 1, 2, &degToRad, lngRad, 1, length);
     
     // Earth coordinates use 0 latitude to refer to the equator,
     //   to convert to cartesian coordinates, 0 should refer to the poles
-    float *absLat = malloc(len * sizeof(float));
-    vvfabsf(absLat, latRad, &len);                    // absLat[n] = | latRad[n] |
-    float halfPi = M_PI_2;
-    vDSP_vsub(absLat, 1, &halfPi, 0, absLat, 1, len); // absLat[n] = M_PI_2 - absLat[n]
-    vvcopysignf(latRad, absLat, latRad, &len);        // latRad[n] = copysign(absLat[n], latRad[n])
+    float *absLat = malloc(length * sizeof(float));
+    vvfabsf(absLat, latRad, &len);                       // absLat = abs(latRad)
+    float const halfPi = M_PI_2;
+    vDSP_vsub(absLat, 1, &halfPi, 0, absLat, 1, length); // absLat = M_PI_2 - absLat
+    vvcopysignf(latRad, absLat, latRad, &len);           // latRad = copysign(absLat, latRad)
     free(absLat);
     
-    float *latSin = malloc(len * sizeof(float));
-    float *latCos = malloc(len * sizeof(float));
+    float *latSin = malloc(length * sizeof(float));
+    float *latCos = malloc(length * sizeof(float));
     vvsincosf(latSin, latCos, latRad, &len);
     
-    float *lngSin = malloc(len * sizeof(float));
-    float *lngCos = malloc(len * sizeof(float));
+    float *lngSin = malloc(length * sizeof(float));
+    float *lngCos = malloc(length * sizeof(float));
     vvsincosf(lngSin, lngCos, lngRad, &len);
     
     
@@ -94,13 +94,15 @@
     vDSP_Length const zOffset = offsetof(SCNVector3, z)/sizeof(float);
     vDSP_Length const dimensions = 3;
     
-    SCNVector3 *cartesian = malloc(len * sizeof(SCNVector3));
-    vDSP_vmul(latSin, 1, lngCos, 1, ((float *)cartesian) + xOffset, dimensions, len); // x
-    vDSP_vmul(latSin, 1, lngSin, 1, ((float *)cartesian) + yOffset, dimensions, len); // y
+    float *latSinRadii = malloc(length * sizeof(float));
+    vDSP_vmul(latSin, 1, radii, 1, latSinRadii, 1, length); // latSinRadii = latSin * radii
     
-    vDSP_vmul(((float *)cartesian) + xOffset, dimensions, radii, 1, ((float *)cartesian) + xOffset, dimensions, len);
-    vDSP_vmul(((float *)cartesian) + yOffset, dimensions, radii, 1, ((float *)cartesian) + yOffset, dimensions, len);
-    vDSP_vmul(latCos, 1, radii, 1, ((float *)cartesian) + zOffset, dimensions, len); // z
+    SCNVector3 *cartesian = malloc(length * sizeof(SCNVector3));
+    vDSP_vmul(latSinRadii, 1, lngCos, 1, ((float *)cartesian) + xOffset, dimensions, length); // cartesian.x = latSinRadii * lngCos
+    vDSP_vmul(latSinRadii, 1, lngSin, 1, ((float *)cartesian) + yOffset, dimensions, length); // cartesian.y = latSinRadii * lngSin
+    vDSP_vmul(      radii, 1, latCos, 1, ((float *)cartesian) + zOffset, dimensions, length); // cartesian.z = radii * latCos
+    
+    free(latSinRadii);
     
     free(lngCos);
     free(latCos);
