927bd7435674312d8882d82a8a810fffce9e8827
Leptos <leptos.0.null@gmail.com>
2021-01-31 11:35:57 -0800

    [NSRange+WSIndex] Add MinIndex and ClampIndex functions

--- WorkoutSpot/Models/NSRange+WSIndex.h
+++ WorkoutSpot/Models/NSRange+WSIndex.h
@@ -8,6 +8,11 @@
 
 #import <Foundation/NSRange.h>
 
+/// The minimum index within the range.
+/// Equivalent to the location
+NS_INLINE NSUInteger NSRangeMinIndex(NSRange range) {
+    return range.location;
+}
 /// The maximum index within the range.
 /// One less than the length plus the location
 NS_INLINE NSUInteger NSRangeMaxIndex(NSRange range) {
@@ -16,4 +21,16 @@ NS_INLINE NSUInteger NSRangeMaxIndex(NSRange range) {
 /// Creates a new @c NSRange with the indicies [startIndex, finalIndex]
 NS_INLINE NSRange NSRangeMakeInclusive(NSUInteger startIndex, NSUInteger finalIndex) {
     return NSMakeRange(startIndex, finalIndex - startIndex + 1);
+}
+/// The closest value to @c indx within @c range
+NS_INLINE NSUInteger NSRangeClampIndex(NSUInteger indx, NSRange range) {
+    NSUInteger min = NSRangeMinIndex(range);
+    if (indx < min) {
+        return min;
+    }
+    NSUInteger max = NSRangeMaxIndex(range);
+    if (indx > max) {
+        return max;
+    }
+    return indx;
 }
