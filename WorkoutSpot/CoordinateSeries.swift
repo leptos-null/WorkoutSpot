292659d4ff3ddcd7db595e232d392c16c333d7de
Leptos <leptos.0.null@gmail.com>
2023-06-17 15:40:58 -0700

    [CoordinateSeries] Implement stepHeight()

--- WorkoutSpot/CoordinateSeries.swift
+++ WorkoutSpot/CoordinateSeries.swift
@@ -64,9 +64,103 @@ final class CoordinateSeries {
 extension CoordinateSeries {
     // the distance between each data point and the value before it.
     // the first value is always 0
-    // func stepHeight() -> ScalarSeries {
-    //
-    // }
+    func stepHeight() -> ScalarSeries {
+        let length = data.count
+        // https://en.wikipedia.org/wiki/Earth_radius#Fixed_radius
+        let a: Double = 6378137 // meters (semi-major radius)
+        let b: Double = 6356752 // meters (semi-minor radius)
+        
+        let aRaiseTwo = a * a // a^2
+        let aRaiseFour = aRaiseTwo * aRaiseTwo // a^4
+        
+        let bRaiseTwo = b * b // b^2
+        let bRaiseFour = bRaiseTwo * bRaiseTwo // b^4
+        
+        let continuousRad = UnsafeMutablePointer<Double>.allocate(capacity: length * 2) // radians
+        let latRad = continuousRad.advanced(by: length * 0) // latitude in radians
+        let lngRad = continuousRad.advanced(by: length * 1) // longitude in radians
+        
+        let latSinCos = UnsafeMutablePointer<Double>.allocate(capacity: length * 2) // sin(latitude), cos(latitude)
+        let latSin = latSinCos.advanced(by: length * 0) // sin(latitude)
+        let latCos = latSinCos.advanced(by: length * 1) // cos(latitude)
+        
+        if let coordinateBase = data.baseAddress {
+            let elementStride = MemoryLayout<Element>.stride / MemoryLayout<Double>.stride
+            var degToRad: Double = .pi / 180
+            
+            vDSP_vsmulD(coordinateBase.pointer(to: \.latitude)!, elementStride, &degToRad, latRad, 1, vDSP_Length(length))
+            vDSP_vsmulD(coordinateBase.pointer(to: \.longitude)!, elementStride, &degToRad, lngRad, 1, vDSP_Length(length))
+        }
+        
+        var latSinBuff = UnsafeMutableBufferPointer(start: latSin, count: length)
+        var latCosBuff = UnsafeMutableBufferPointer(start: latCos, count: length)
+        
+        vForce.sincos(
+            UnsafeBufferPointer(start: continuousRad, count: length * 2),
+            sinResult: &latSinBuff,
+            cosResult: &latCosBuff
+        )
+        
+        let latSinSqCosSq = UnsafeMutablePointer<Double>.allocate(capacity: length * 2) // sin^2(latitude), cos^2(latitude)
+        let latSinSq = continuousRad.advanced(by: length * 0) // sin^2(latitude)
+        let latCosSq = continuousRad.advanced(by: length * 1) // cos^2(latitude)
+        
+        var latSinSqCosSqBuff = UnsafeMutableBufferPointer(start: latSinSqCosSq, count: length * 2)
+        let latSinCosBuff = UnsafeMutableBufferPointer(start: latSinCos, count: length * 2)
+        vDSP.square(latSinCosBuff, result: &latSinSqCosSqBuff)
+        
+        var radiusNum = UnsafeMutableBufferPointer<Double>.allocate(capacity: length)
+        vDSP.add(
+            multiplication: (UnsafeBufferPointer(start: latCosSq, count: length), aRaiseFour),
+            multiplication: (UnsafeBufferPointer(start: latSinSq, count: length), bRaiseFour),
+            result: &radiusNum
+        )
+        
+        var radiusDenom = UnsafeMutableBufferPointer<Double>.allocate(capacity: length)
+        vDSP.add(
+            multiplication: (UnsafeBufferPointer(start: latCosSq, count: length), aRaiseTwo),
+            multiplication: (UnsafeBufferPointer(start: latSinSq, count: length), bRaiseTwo),
+            result: &radiusDenom
+        )
+        
+        var radii = UnsafeMutableBufferPointer<Double>.allocate(capacity: length)
+        vDSP.divide(radiusNum, radiusDenom, result: &radii)
+        vForce.sqrt(radii, result: &radii)
+        
+        var averageSpacing = UnsafeMutableBufferPointer<Double>.allocate(capacity: length * 2)
+        vDSP.multiply(subtraction: (
+            UnsafeBufferPointer(start: continuousRad.advanced(by: 1), count: length * 2 - 1),
+            UnsafeBufferPointer(start: continuousRad, count: length * 2 - 1)
+        ), 0.5, result: &averageSpacing[1...])
+        averageSpacing[length * 0] = 0
+        averageSpacing[length * 1] = 0 // this is the point where `averageSpacing[n] = lngRad[0] - latSin[n - 1]`
+        
+        // sin(0) = 0 so we don't have to worry about the two cases above
+        vForce.sin(averageSpacing, result: &averageSpacing)
+        vDSP.square(averageSpacing, result: &averageSpacing)
+        
+        let latSpacing = averageSpacing[(length*0)..<(length*1)]
+        let lngSpacing = averageSpacing[(length*1)..<(length*2)]
+        
+        var values = UnsafeMutableBufferPointer<Double>.allocate(capacity: length)
+        values[0] = 0
+        vDSP.multiply(latCosBuff[1...], latCosBuff.dropLast(), result: &values[1...])
+        vDSP.add(multiplication: (values, lngSpacing), latSpacing, result: &values)
+        vForce.sqrt(values[1...], result: &values[1...])
+        vForce.asin(values[1...], result: &values[1...])
+        vDSP.multiply(addition: (radii[1...], radii.dropLast()), values[1...], result: &values[1...])
+        
+        averageSpacing.deallocate()
+        radii.deallocate()
+        radiusDenom.deallocate()
+        radiusNum.deallocate()
+        
+        latSinSqCosSq.deallocate()
+        latSinCos.deallocate()
+        continuousRad.deallocate()
+        
+        return ScalarSeries(raw: values)
+    }
 }
 
 extension CoordinateSeries {
