0880f724c9310431c46d01bf4786a8c71619b4b8
Leptos <leptos.0.null@gmail.com>
2024-10-03 19:08:39 -0700

    [WSIcon] Add target for drawing app icon

---
+++ WSIcon/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WSIcon
+//
+//  Created by Leptos on 10/3/24.
+//  Copyright © 2024 Leptos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WSIcon/WSIconApp.swift
@@ -0,0 +1,18 @@
+//
+//  WSIconApp.swift
+//  WSIcon
+//
+//  Created by Leptos on 10/3/24.
+//  Copyright © 2024 Leptos. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WSIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
